546c5b012b754dd4aa3ecd1714dd760d49c047ba
Garrett Boone <Boone.Garrett@gmail.com>
2016-09-29 23:06:17 -0500

    cors

--- server/app.js
+++ server/app.js
@@ -21,8 +21,24 @@ mongoose.connection.on('error', function(err) {
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
 // Setup server
 var app = express();
+
+app.use(allowCrossDomain);
+
 var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
   serveClient: config.env !== 'production',
