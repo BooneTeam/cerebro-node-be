52dca4eb8704f39fa6da5172e9c39d7a934ff0cb
Garrett Boone <Boone.Garrett@gmail.com>
2016-12-06 14:41:48 -0600

    wtf

--- server/routes.js
+++ server/routes.js
@@ -9,16 +9,32 @@ var cors = require('cors');
 
 module.exports = function(app) {
   // Insert routes below
-  var corsOptions = {
-    origin: 'http://cerebro-dbc.herokuapp.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  // var corsOptions = {
+  //   origin: 'http://cerebro-dbc.herokuapp.com',
+  //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  // };
+  //
+  // if(process.env.NODE_ENV == 'development'){
+  //   corsOptions['origin'] = ['http://cerebro-dbc.herokuapp.com', 'http://localhost:8080']
+  // }
+
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
   };
+  app.use(allowCrossDomain);
 
-  if(process.env.NODE_ENV == 'development'){
-    corsOptions['origin'] = ['http://cerebro-dbc.herokuapp.com', 'http://localhost:8080']
-  }
-
-  app.use(cors(corsOptions));
+  // app.use(cors(corsOptions));
+  // debugger;
   app.use('/api/activities', require('./api/activity'));
   app.use('/api/repos', require('./api/repo'));
   app.use('/api/things', require('./api/thing'));
